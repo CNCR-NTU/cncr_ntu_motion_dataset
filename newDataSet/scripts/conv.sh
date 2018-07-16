18294d650eaa89321446f25f40532ed8f378a469
pedro_machado <pedro.m@sundance.com>
2018-07-16 20:23:50 +0100

    update

--- newDataSet/scripts/conv.sh
+++ newDataSet/scripts/conv.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
 echo "Resizing images to 40x40"
 mkdir -p ../objects
-for I in {1..100}
+angle=0
+for K in 'left-right' 'up-down' 'right-left' 'down-up'
 do
-	convert "../rawImages/HzMovSet1/img"$I".png"  -resize 40x40\! "../objects/img"$I".png"
-	convert "../objects/img"$I".png" -colorspace Gray -separate -average "../objects/img"$I".png"
+    for J in {1..100}
+    do
+        mkdir -p "../objects/"$K"/seq"$J
+        for I in {1..50}
+        do
+                convert "../rawImages/HzMovSet1/img"$I".png"  -resize 40x40\! "../objects/"$K"/seq"$J"/img"$I".png"
+                convert "../objects/"$K"/seq"$J"/img"$I".png" -colorspace Gray -separate -average "../objects/"$K"/seq"$J"/img"$I".png"
+                convert -rotate $angle "../objects/"$K"/seq"$J"/img"$I".png" "../objects/"$K"/seq"$J"/img"$I".png"
+        done
+    done
+    angle=$((angle+90))
+    echo $angle
 done
-
- 
